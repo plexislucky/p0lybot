906d0eb1c02cc1b6e6e87aeeaeba833f840b7166
plexislucky <109311001+plexislucky@users.noreply.github.com>
2022-07-18 00:17:24 +0300

    added window movement functionality
    lel bois

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import ctypes, ctypes.wintypes
 import win32gui
 import pyautogui
 import time
@@ -8,21 +9,24 @@ import json
 # TODO: add keylogger-esque method to register killswitch key when console window not active
 # TODO: try not to skid lel
 
-macroActiveConsoleState = """
-       pika thanks u for using p0lybot
-               ,___          .-;'
-               `"-.`\_...._/`.`
-            ,      \        /
-         .-' ',    / ()   ()\\
-        `'._   \  /()    .  (|       https://twitter.com/plexalwayslucky
-            > .' ;,     -'-  /       
-           / <   |;,     __.;
-           '-.'-.|  , \    , \\
-              `>.|;, \_)    \_)
-               `-;     ,    /
-                  \    /   <
-                   '. <`'-,_)
-                    '._)"""
+macroActiveConsoleState = """         
+       .-.,     ,.-.                ___  _       _           _  
+  '-.  /:::\\   //:::\  .-'         / _ \| |     | |         | |  
+ '-.\|':':' `"` ':':'|/.-'   _ __ | | | | |_   _| |__   ___ | |_ 
+ `-./`. .-=-. .-=-. .`\.-`  | '_ \| | | | | | | | '_ \ / _ \| __|
+   /=- /     |     \ -=\    | |_) | |_| | | |_| | |_) | (_) | |_         
+  ;   |      |      |   ;   | .__/ \___/|_|\__, |_.__/ \___/ \__|
+  |=-.|______|______|.-=|   | |             __/ |                
+  |==  \  0 /_\ 0  /  ==|   |_|            |___/                 
+  |=   /'---( )---'\   =|
+   \   \:   .'.   :/   /         
+    `\= '--`   `--' =/'           
+      `-=._     _.=-'
+           `\"\"\"`
+
+    https://github.com/plexislucky/p0lybot
+    https://twitter.com/plexalwayslucky
+           """
 def windowEnumerationHandler(hwnd, top_windows):
     top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
 
@@ -33,6 +37,17 @@ def windowName(name): # lazy
 
 clear = lambda: os.system('cls') # lazy again
 
+user32 = ctypes.windll.user32
+
+def growtopiaWindowMove (x, y):
+    hwnd = user32.FindWindowW(None, u'Growtopia')
+    rect = ctypes.wintypes.RECT()
+    user32.GetWindowRect(hwnd, ctypes.pointer(rect))
+
+    w = rect.right - rect.left
+    h = rect.bottom - rect.top
+
+    user32.MoveWindow(hwnd, x, y, w ,h)
 
 def growtopiaWindowActivate(): # pasted function idc
     results = []
@@ -60,6 +75,7 @@ def doFarmLoop(walkLength, punchCount):
     windowName("p0lybot - Active")
     print(macroActiveConsoleState)
     growtopiaWindowActivate()
+    growtopiaWindowMove(0, 0)
     time.sleep(0.5)
     while True:
         sendKey("space", 0, 3)
